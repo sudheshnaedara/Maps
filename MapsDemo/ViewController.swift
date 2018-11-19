b87d2b26a73475e5e3fcd17a502aa2fd1d668111
Edara Sudeshna Devi <edara.devi@spark.co.nz>
2018-11-19 11:45:03 +0530

    Initial Commit

---
+++ MapsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapsDemo
+//
+//  Created by SparkMac on 19/11/18.
+//  Copyright © 2018 Infosys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
